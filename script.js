8f54007a8a15a1d93940b79d854ec64a9f4e63a0
Optimus <94816671+Optimuspime123@users.noreply.github.com>
2025-02-09 01:26:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ const messages = [
     "Are you sure?",
     "Really sure??",
     "Are you positive?",
-    "Pookie please...",
+    "Baby please...",
     "Just think about it!",
     "If you say no, I will be really sad...",
     "I will be very sad...",
+    "I will hit u with stick",
     "I will be very very very sad...",
     "Ok fine, I will stop asking...",
     "Just kidding, say yes please! ❤️"
